0acad5bbf59be0c75d464050c758948d20fc555c
Aleksey Kudryavtsev <st1gii@yahoo.com>
2022-08-10 17:58:33 +0300

    Go study DATA

---
+++ PythonStudy/variables.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data = {'col 1': ['Я', 'Python', 'Буду'], 'col 2': ['люблю', 'мой', 'стараться'], 'col 3': ['анализ', 'лучший', 'хорошо'], 'col 4': ['данных', 'друг', 'учиться']}
+df = pd.DataFrame(data)
+print(df)
